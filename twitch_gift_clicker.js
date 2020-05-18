7bdeb6e8ce7b56f9c905b8b6996edec228a646d5
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-05-18 20:34:25 -0300

    Added header

--- twitch_gift_clicker.js
+++ twitch_gift_clicker.js
@@ -1,4 +1,8 @@
-// Click gift twitch button every 60 seconds
+// Author: Erik Kazuo Sugawara
+// Github: https://github.com/ErikSugawara
+// Release: May 18, 2020
+// Version: 1.0
+// Description: A simple script that autoclick a gift button on twitch.tv website.
 
 function gift_clicker() {
 	if (document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0]){
@@ -9,4 +13,3 @@ function gift_clicker() {
 }
 var seconds = 60;
 setInterval(function(){gift_clicker()}, seconds * 1000);
-
