2ee5845950c401c6464df0e9a8ff3206532eba1d
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-05-18 19:38:56 -0300

    Fixed some bugs

--- twitch_gift_clicker.js
+++ twitch_gift_clicker.js
@@ -1,7 +1,12 @@
 // Click gift twitch button every 60 seconds
+
 function gift_clicker() {
-	document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0].click();
-	console.log("Button Clicked");
+	if (document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0]){
+		document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0].click();
+		console.log("Button Clicked!");
+	}
+	console.log("Gift button not ready")
 }
-var timer_id = setInterval(gift_clicker(), 60*1000)
+var seconds = 60;
+setInterval(function(){gift_clicker()}, seconds * 1000);
 
