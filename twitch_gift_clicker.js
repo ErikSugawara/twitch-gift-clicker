9d21aeaf6234ab49b264a33d0777f2dda42b860d
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-07-03 18:08:30 -0300

    Removed unnecessary info/refresh rate

--- twitch_gift_clicker.js
+++ twitch_gift_clicker.js
@@ -1,15 +1,11 @@
 // Author: Erik Kazuo Sugawara
-// Github: https://github.com/ErikSugawara
 // Release: May 18, 2020
-// Version: 1.0
+// Version: 1.1.1
 // Description: A simple script that autoclick a gift button on twitch.tv website.
 
 function gift_clicker() {
-	if (document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0]){
-		document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0].click();
-		console.log("Button Clicked!");
-	}
-	console.log("Gift button not ready")
+	document.getElementsByClassName('tw-button tw-button--success tw-interactive')[0].click();
 }
-var seconds = 60;
+
+var seconds = 5;
 setInterval(function(){gift_clicker()}, seconds * 1000);
